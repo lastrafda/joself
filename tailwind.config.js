d087e9652aecb01218233e8b7dd4d1b0d75d97f7
José Lastra <lastrafda@gmail.com>
2020-04-18 23:28:04 -0400

    added text colors for twitter

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   theme: {
     extend: {
+      colors: {
+        poison: {
+          main: '#00b300'
+        },
+        twitter: {
+          main: '#1da1f2'
+        }
+      }
     },
   },
   variants: {},
