dac7e108629950b8fb939c5c28be4704e54a77d3
José Lastra <lastrafda@gmail.com>
2020-04-18 23:31:28 -0400

    removed title

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="py-8 px-4">
-    <h1 class="text-center text-yellow-600 text-3xl font-bold pb-8">Please have a seat</h1>
     <post-item>
       <template v-slot:title>
         <router-link to="/posts/shoshin" class="underline">Shoshin ⇦</router-link>
